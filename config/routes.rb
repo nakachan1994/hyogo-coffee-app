18dae8b35627316c44800371bea52abb1de0253c
nakachan1994 <nakachaaaan@gmail.com>
2021-05-29 03:00:18 +0000

    [add]admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  devise_for :admins
   devise_for :users
   root to: 'homes#index'
+
+  namespace :admin do
+    resources :products, only: [:new, :create, :index, :show, :edit, :update]
+  end
+  namespace :public do
+    resources :products, only: [:index, :show]
+  end
+
 end
